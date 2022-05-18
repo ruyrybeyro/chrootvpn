9a21b220d3a2babd05cfce3f5a43d2c68b252662
Rui Ribeiro <ruyrybeyro@gmail.com>
2022-05-18 15:45:55 +0100

    CentOS fixes

--- vpn.sh
+++ vpn.sh
@@ -793,6 +793,7 @@ needCentOSFix()
    then
       sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
       sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
+       yum -y install epel-release || die "could not install epel-release"
    else
       die "could not do yum. Fix it"
    fi
