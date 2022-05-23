3d83466f41002d5c3f83ee867cccaeea118cde58
Rui Ribeiro <ruyrybeyro@gmail.com>
2022-05-23 10:25:50 +0100

    status: show CShell self-signed CA certificate

--- vpn.sh
+++ vpn.sh
@@ -478,11 +478,22 @@ showStatus()
       wget -q -O- --no-check-certificate "https://${VPN}/${SSLVPN}/SNX/CSHELL/snx_ver.txt" 2> /dev/null || echo "Could not get SNX download version" >&2
    fi
 
+   # Mobile Access Portal Agent
+   echo
    echo -n "CShell - available for download "
 
    if ! wget -q -O- --no-check-certificate "https://${VPN}/SNX/CSHELL/cshell_ver.txt" 2> /dev/null
    then
       wget -q -O- --no-check-certificate "https://${VPN}/${SSLVPN}/SNX/CSHELL/cshell_ver.txt" 2> /dev/null || echo "Could not get SNX download version" >&2
+   fi
+
+   # Mobile Access Portal Agent X.509 self-signed CA certificate
+   echo
+   echo "CShell self-signed CA certificate"
+   echo
+   if [[ -f "${CHROOT}/usr/bin/cshell/cert/CShell_Certificate.crt" ]]
+   then
+      openssl x509 -in "${CHROOT}/usr/bin/cshell/cert/CShell_Certificate.crt" -text | head -14 | tail +7
    fi
 
    # IP connectivity
