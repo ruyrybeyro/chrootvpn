483a70bfe6d86e928f483d2f0a0c27aaa10b7831
Rui Ribeiro <ruyrybeyro@gmail.com>
2022-10-20 04:51:05 +0100

    bump version

--- vpn.sh
+++ vpn.sh
@@ -35,7 +35,7 @@
 # BEWARE of cut&paste or code beautifiers, it will break code.
 
 # script/deploy version, make the same as deploy
-VERSION="v1.94"
+VERSION="v1.95"
 
 # default configuration file
 # created first time upon successful setup/run
