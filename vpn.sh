a5f3772b84dd062ccb34b2329b42c0eebfb6be04
Rui Ribeiro <ruyrybeyro@gmail.com>
2022-12-15 10:57:26 +0000

    change order of curl parameters

--- vpn.sh
+++ vpn.sh
@@ -2243,17 +2243,17 @@ buildFS()
    # rm -f snx_install.sh cshell_install.sh 2> /dev/null
 
    # downloads SNX installation scripts from CheckPoint machine
-   if curl -k --output "${CHROOT}/root/snx_install.sh" --fail --silent "https://${VPN}/SNX/INSTALL/snx_install.sh"
+   if curl -k --fail --silent --output "${CHROOT}/root/snx_install.sh" "https://${VPN}/SNX/INSTALL/snx_install.sh"
    then 
       # downloads CShell installation scripts from CheckPoint machine
-      curl -k --output "${CHROOT}/root/cshell_install.sh" --silent "https://${VPN}/SNX/INSTALL/cshell_install.sh" || die "could not download cshell_install.sh" 
+      curl -k --fail --silent --output "${CHROOT}/root/cshell_install.sh" "https://${VPN}/SNX/INSTALL/cshell_install.sh" || die "could not download cshell_install.sh" 
       # registers CShell installed version for later
       curl -k --fail --silent "https://${VPN}/SNX/CSHELL/cshell_ver.txt" 2> /dev/null > root/.cshell_ver.txt 
    else
       # downloads SNX installation scripts from CheckPoint machine
-      curl -k --output "${CHROOT}/root/snx_install.sh" --fail "https://${VPN}/${SSLVPN}/SNX/INSTALL/snx_install.sh" || die "could not download snx_install.sh" 
+      curl -k --fail --silent --output "${CHROOT}/root/snx_install.sh" "https://${VPN}/${SSLVPN}/SNX/INSTALL/snx_install.sh" || die "could not download snx_install.sh" 
       # downloads CShell installation scripts from CheckPoint machine
-      curl -k --output "${CHROOT}/root/cshell_install.sh" --silent --fail "https://${VPN}/${SSLVPN}/SNX/INSTALL/cshell_install.sh" || die "could not download cshell_install.sh" 
+      curl -k --fail --silent --output "${CHROOT}/root/cshell_install.sh" "https://${VPN}/${SSLVPN}/SNX/INSTALL/cshell_install.sh" || die "could not download cshell_install.sh" 
       # registers CShell installed version for later
       curl -k --silent --fail "https://${VPN}/${SSLVPN}/SNX/CSHELL/cshell_ver.txt" 2> /dev/null > root/.cshell_ver.txt
    fi
